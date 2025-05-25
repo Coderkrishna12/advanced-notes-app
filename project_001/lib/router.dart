66542bdc14a1424d731862464a917be53e42529f
Coderkrishna12 <krishnajaiswal1013@gmail.com>
2025-05-25 14:43:47 +0530

    feat: add initial implementation of AppRouter with basic routing setup

---
+++ project_001/lib/router.dart
@@ -0,0 +1,10 @@
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, initial: true),
+        // Add other routes later
+      ];
+}
