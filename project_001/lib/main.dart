dd08adf5a91b78ffa422c149877b7946bdea1533
Coderkrishna12 <krishnajaiswal1013@gmail.com>
2025-05-25 14:39:48 +0530

    refactor: update main function to initialize Hive and clean up pubspec.yaml

--- project_001/lib/main.dart
+++ project_001/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
